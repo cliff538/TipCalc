5e03429a79861014d7584947b302cdba2d6a198d
cliff538 <cliff538@yahoo.com>
2017-11-08 18:43:06 -0700

    working app. still have layout issues.

--- TipCalc/ViewController.swift
+++ TipCalc/ViewController.swift
@@ -17,23 +17,34 @@ class ViewController: UIViewController {
 		createToolbar()
 		
 		
-	}
-	
-	// MARK: - Outlets
+	}// MARK: - Outlets
 	@IBOutlet weak var subtotalTextField: UITextField!
 	@IBOutlet weak var tipPercentLabel: UILabel!
+	@IBOutlet weak var tipPercentSlider: UISlider!
+	@IBOutlet weak var tipAmountLabel: UILabel!
+	@IBOutlet weak var totalAmountLabel: UILabel!
+	
+	// MARK: - Properties
+	let model = Model()
+	
+	
 	
 	// MARK: - Interactions
 	@IBAction func dragSlider(_ sender: UISlider) {
 		//testing: print("\(Int(sender.value))")
 		
 		tipPercentLabel.text = "Tip (\(Int(sender.value))%):"
+		
+		model.tipPercentFromSlider = Int(sender.value)
+		updateLabels()
 	}
 	
 	
 	
 	
 	// MARK: - Functions
+	
+	//creating a toolbar that sits above keyboard 
 	func createToolbar() {
 		let toolbar = UIToolbar()
 		toolbar.barStyle = .default
@@ -60,13 +71,23 @@ class ViewController: UIViewController {
 	@objc func dismissKeyboard() {
 		subtotalTextField.resignFirstResponder() //removes keyboard
 		
+		if subtotalTextField.text?.characters.count == 0 {
+			subtotalTextField.text = "$0.00"
+			model.subtotalFromTextField = "0.00"
+			updateLabels()
+		} else {
+			model.subtotalFromTextField = subtotalTextField.text!
+			model.tipPercentFromSlider = Int(tipPercentSlider.value)
+			
+			updateLabels()
+		}
+		
 	}
 	
-	
-	
-
-	
-
-
+	func updateLabels() {
+		subtotalTextField.text = model.subtotalAsCurrency
+		tipAmountLabel.text = model.tipAmountAsCurrency
+		totalAmountLabel.text = model.totalAmountAsCurrency
+	}
 }
 
